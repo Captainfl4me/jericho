77ca3a0cdd5f450e19d6c54e111dae4db1067f71
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2023-10-08 22:09:08 +0200

    rmv: build folder

--- KalmanTest/build/CMakeFiles/kalmantest.dir/C_/Program_Files/Raspberry_Pi/Pico_SDK_v1.5.1/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles\kalmantest.dir\C_\Program_Files\Raspberry_Pi\Pico_SDK_v1.5.1\pico-sdk\src\rp2_common\pico_standard_link\new_delete.cpp.obj: \
- C:\Program\ Files\Raspberry\ Pi\Pico\ SDK\ v1.5.1\pico-sdk\src\rp2_common\pico_standard_link\new_delete.cpp
